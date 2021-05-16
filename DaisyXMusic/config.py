e88229cfd043add471e2c640d03459d117b7a90e
Soheib1378 <77415087+Soheib1378@users.noreply.github.com>
2021-05-17 02:22:21 +0430

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -24,16 +24,15 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = "BAB6ynfiva2JMNojF5TFU73qqTgQnxZ1RXlk3A8zcC_MitmkOQExcjOO3Ic0oy4F1MZ0BhnBr3RM-s-tozhsIw47S_97cKGBOCWS0OrUMOJ90wFMkL1rYmlI9DrvK9EYjSsJAffV8jJKOwwIMOlJ5iGxkIvi7mSGN4MiDaAVdAydvVH9tMgY0TD8WMRRtoLWR9MIYhhvuJkM_0qpLZkja1harhJIh4z2orBU4mZSIHQN6j0jYoiV_PeMx5QZEKkgz8iYAeo1TPhy42x6ZLciNfMn9X_oJLPZVIY0hUYxwY0xvdbijtLDs_0Lgc6F5FdqRRCXKfnhnwer2Hlu-zbzYybgYzw9UQA"
+BOT_TOKEN = "1785461638:AAFM9uQeeBkh-yD6lGpYxLGKNHwDrf2HYTQ"
+BOT_NAME = "Music Player"
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "@DaisyXupdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
+BG_IMAGE = "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png"
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
-DURATION_LIMIT = 4
+API_ID = 4328913
+API_HASH = "3230ec801f78a517c9a2ad6bebb7f7b4"
+DURATION_LIMIT = 7
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
