0e240e7344cdcaa1ab2037dabc8b1916564e8aa2
Soheib1378 <77415087+Soheib1378@users.noreply.github.com>
2021-05-16 23:20:04 +0430

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -33,7 +33,7 @@ admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("7", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
