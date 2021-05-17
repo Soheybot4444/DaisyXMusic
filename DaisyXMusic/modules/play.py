2c98de2cf6431b8033d9c07658ec1b60fa2a32d8
Soheybot4444 <78666145+Soheybot4444@users.noreply.github.com>
2021-05-17 06:18:11 +0430

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -239,9 +239,9 @@ async def p_cb(b, cb):
             temp.append(t)
         now_playing = temp[0][0]
         by = temp[0][1].mention(style="md")
-        msg = "**Now Playing** in {}".format(cb.message.chat.title)
+        msg = "{} در حال پخش در".format(cb.message.chat.title)
         msg += "\n- " + now_playing
-        msg += "\n- Req by " + by
+        msg += "به درخواست" + by
         temp.pop(0)
         if temp:
             msg += "\n\n"
@@ -307,9 +307,9 @@ async def m_cb(b, cb):
             temp.append(t)
         now_playing = temp[0][0]
         by = temp[0][1].mention(style="md")
-        msg = "**Now Playing** in {}".format(cb.message.chat.title)
+        msg = "{} در حال پخش در".format(cb.message.chat.title)
         msg += "\n- " + now_playing
-        msg += "\n- Req by " + by
+        msg += "به درخواست" + by
         temp.pop(0)
         if temp:
             msg += "\n\n"
